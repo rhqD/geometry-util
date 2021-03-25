139cd438a49a218d0468c1bab787d987c67ec85d
rhq <renhanquandadi@gmail.com>
2021-03-25 14:13:36 +0800

    restruct folder

--- src/vector/getAngleBetweenTwoVectors.ts
+++ src/vector/getAngleBetweenTwoVectors.ts
@@ -1,6 +1,6 @@
 import { IVector } from "../types";
 import getRadiusBetweenVectors from "./getRadiusBetweenVectors";
-import radiusToAngle from "./radiusToAngle";
+import radiusToAngle from "../common/radiusToAngle";
 
 const getAngleBetweenTwoVectors = (vector1: IVector, vector2: IVector): number => {
   const radius = getRadiusBetweenVectors(vector1, vector2);
